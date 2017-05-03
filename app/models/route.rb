d89473f6c8da94504267adc0599855b134e62498
Badichan Polina <badichan@mail.ru>
2017-05-03 23:46:36 +0200

    lesoon-15fix

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,5 +2,6 @@ class Route < ApplicationRecord
   validates :name, presence: true
 
   has_many :trains
-  has_and_belongs_to_many :railway_stations
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
 end
