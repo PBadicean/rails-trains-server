4c6578abec1f9369a3034f1cf90af6f3d65ee280
Badichan Polina <badichan@mail.ru>
2017-06-08 15:11:26 +0200

    mailer to delete ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,7 +5,9 @@ class Ticket < ApplicationRecord
   belongs_to :start_station, class_name: 'RailwayStation'
   belongs_to :end_station, class_name: 'RailwayStation'
 
-  after_create :send_notification
+  after_create :send_buy_notification
+  after_destroy :send_delete_notification
+
 
   def route_name
     "#{start_station.title} - #{end_station.title}"
@@ -13,7 +15,12 @@ class Ticket < ApplicationRecord
 
   private
 
-  def send_notification
+  def send_delete_notification
+    TicketsMailer.delete_ticket(self.user, self).deliver_now
+  end
+
+  def send_buy_notification
     TicketsMailer.buy_ticket(self.user, self).deliver_now
   end
+
 end
